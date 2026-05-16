2152cf379da27ba5597ca3942abcf7b4d7241b6a
moniquomosende <moniquonicole.mosende@cit.edu>
2026-05-16 12:39:36 +0800

    Add test for Transaction model creation
    Added a test case for creating a Transaction model instance.

--- finances/tests.py
+++ finances/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Transaction
 
-# Create your tests here.
+
+class TransactionModelTest(TestCase):
+
+    def test_create_transaction(self):
+        transaction = Transaction.objects.create(
+            title="Salary",
+            amount=1000,
+            transaction_type="Income",
+            description="Monthly Salary"
+        )
+
+        self.assertEqual(transaction.title, "Salary")
